d102f95fd519af90f200ef97aefa221e90ec6ec1
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-09-29 12:51:11 +0800

    Imported upstream version '3.8.1' of 'upstream'

--- include/robot_localization/navsat_conversions.hpp
+++ include/robot_localization/navsat_conversions.hpp
@@ -36,7 +36,6 @@
 * https://github.com/austin-robot/utexas-art-ros-pkg/blob/afd147a1eb944fc3dbd138574c39699813f797bf/stacks/art_vehicle/art_common/include/art/UTM.h
 * https://github.com/austin-robot/utexas-art-ros-pkg/blob/afd147a1eb944fc3dbd138574c39699813f797bf/stacks/art_vehicle/art_common/include/art/conversions.h
 */
-
 #ifndef ROBOT_LOCALIZATION__NAVSAT_CONVERSIONS_HPP_
 #define ROBOT_LOCALIZATION__NAVSAT_CONVERSIONS_HPP_
 
@@ -51,11 +50,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <GeographicLib/MGRS.hpp>
-#include <GeographicLib/UTMUPS.hpp>
-
 #include <cmath>
 #include <string>
+
+#include <GeographicLib/MGRS.hpp>
+#include <GeographicLib/UTMUPS.hpp>
 
 namespace robot_localization
 {
